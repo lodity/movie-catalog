9665dca4afa3df7467c4ed1fee2405f165df9f47
lodity <lodity9@gmail.com>
2022-10-05 17:39:48 +0300

    add CardPlaceholder

---
+++ src/components/UI/CardPlaceholder/CardPlaceholder.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './CardPlaceholder.module.css';
+
+const CardPlaceholder = () => {
+	return (
+		<div className={classes.cardPlaceholder}>
+			<div className={classes.cardPlaceholderRating}></div>
+			<div className={classes.cardPlaceholderPoster}></div>
+			<div className={classes.cardPlaceholderTitle}></div>
+		</div>
+	);
+};
+
+export default CardPlaceholder;
