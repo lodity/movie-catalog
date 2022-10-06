a5ef3a2fe6a42ef63c369076cf58634b06fb654f
lodity <lodity9@gmail.com>
2022-10-06 10:59:58 +0300

    movie wrapper deleted

--- src/components/PopularMoviesContainer.tsx
+++ src/components/PopularMoviesContainer.tsx
@@ -3,20 +3,18 @@ import { movieAPI } from '../services/MovieService';
 import MovieCard from './UI/MovieCard/MovieCard';
 
 const PopularMoviesContainer = () => {
-	const { data: data } = movieAPI.usePopularMoviesQuery(1);
+	const { data } = movieAPI.usePopularMoviesQuery(1);
 	if (data) console.log(data);
 
 	return (
-		<div className="movies">
-			<div className="movies__container">
-				<div className="movies__content">
-					{data &&
-						data.results
-							.filter((movie) => movie.poster_path !== null)
-							.map((movie) => (
-								<MovieCard key={movie.id} movie={movie} />
-							))}
-				</div>
+		<div className="movies__container">
+			<div className="movies__content">
+				{data &&
+					data.results
+						.filter((movie) => movie.poster_path !== null)
+						.map((movie) => (
+							<MovieCard key={movie.id} movie={movie} />
+						))}
 			</div>
 		</div>
 	);
