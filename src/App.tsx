71dded4021fbb476ab157dd98d27c26a212ae07a
lodity <lodity9@gmail.com>
2022-10-07 20:40:43 +0300

    react router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import PopularMoviesContainer from './components/PopularMoviesContainer';
 import './assets/styles/App.css';
 import './assets/styles/fonts.css';
 import './assets/styles/nullstyle.css';
+import Navbar from './components/UI/Navbar/Navbar';
+import { BrowserRouter } from 'react-router-dom';
+import AppRouter from './components/AppRouter';
 
 function App() {
 	return (
-		<div className="App">
-			<PopularMoviesContainer />
-		</div>
+		<BrowserRouter>
+			<Navbar />
+			<div className="App">
+				<AppRouter />
+			</div>
+		</BrowserRouter>
 	);
 }
 
