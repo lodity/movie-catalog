4a654effb34922a95df9329cbc521b9fa7f2c892
lodity <lodity9@gmail.com>
2022-10-12 21:13:23 +0300

    useObserver hook added

---
+++ src/hooks/useObserver.ts
@@ -0,0 +1,22 @@
+import { useRef } from 'react';
+
+export const useObserver = (
+	ref: any,
+	canLoad: boolean,
+	isLoading: boolean,
+	callback: any
+) => {
+	const observer: any = useRef(null);
+	if (isLoading) return;
+	if (observer.current) {
+		observer.current.disconnect();
+	}
+	let cb = function (entries: any[], observer: any) {
+		if (entries[0].isIntersecting && canLoad) {
+			callback();
+		}
+	};
+
+	observer.current = new IntersectionObserver(cb);
+	observer.current.observe(ref.current);
+};
