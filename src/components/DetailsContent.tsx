c6f4911bdf598058c2ca3d6d946e1d5711fee448
lodity <lodity9@gmail.com>
2023-04-30 20:39:35 +0300

    Added the ability to display TV series in DetailsContent

--- src/components/DetailsContent.tsx
+++ src/components/DetailsContent.tsx
@@ -5,13 +5,19 @@ import RatingButton from './UI/RatingButton/RatingButton';
 
 const DetailsContent: FC = () => {
 	const linkId = useParams();
+	let type: string | string[] = linkId.type ? linkId.type.split('') : [''];
+	let id: number | string[] = linkId.id ? linkId.id.split('') : ['1'];
 
-	let movieId: number | string[] = linkId.id ? linkId.id.split('') : ['1'];
-	movieId.shift();
-	movieId = +movieId.join('');
-
-	const { data, isLoading, error } = theMovieDBAPI.useMovieQuery(movieId);
-	if (data) console.log(data);
+	type.shift();
+	type = type.join('');
+	id.shift();
+	id = +id.join('');
+	console.log(type);
+	const { data, isLoading, error } =
+		type === 'Movie'
+			? theMovieDBAPI.useMovieQuery(id)
+			: theMovieDBAPI.useTvQuery(id);
+	if (data) console.log(data.status);
 
 	return (
 		<div>
@@ -24,7 +30,9 @@ const DetailsContent: FC = () => {
 						/>
 					</div>
 					<div className="content-details__title-block">
-						<h1 className="content-details__title">{data.title}</h1>
+						<h1 className="content-details__title">
+							{type === 'Movie' ? data.title : data.name}
+						</h1>
 					</div>
 					<div className="content-details__main-block main-block">
 						<div className="main-block__poster">
@@ -46,29 +54,81 @@ const DetailsContent: FC = () => {
 								/>
 							</div>
 							<div className="main-block__infoBlock">
-								<div className="main-block__type">
-									<p className="main-block__type-title infoTitle">
-										Type
-									</p>
-									<p className="main-block__type-value infoValue">
-										Type
-									</p>
-								</div>
-								<div className="main-block__release">
-									<p className="main-block__release-title infoTitle">
-										Release Date:
-									</p>
-									<p className="main-block__release-value infoValue">
-										{data.release_date}
-									</p>
-								</div>
-								<div className="main-block__run-time">
-									<p className="main-block__run-time-title infoTitle">
-										Run time
-									</p>
-									<p className="main-block__run-time-value infoValue">
-										{data.runtime} min
-									</p>
+								<div className="main-block__left_right">
+									<div className="main-block__left">
+										<div className="main-block__type">
+											<p className="main-block__type-title infoTitle">
+												Type
+											</p>
+											<p className="main-block__type-value infoValue">
+												{type}
+											</p>
+										</div>
+										<div className="main-block__release">
+											<p className="main-block__release-title infoTitle">
+												{type === 'Movie'
+													? 'Release Date'
+													: 'First air date'}
+											</p>
+											<p className="main-block__release-value infoValue">
+												{type === 'Movie'
+													? data.release_date
+													: data.first_air_date}
+											</p>
+										</div>
+										{type === 'tv' && (
+											<div className="main-block__seasons_num">
+												<p className="main-block__seasons-title infoTitle">
+													No. of Seasons
+												</p>
+												<p className="main-block__seasons-value infoValue">
+													{data.number_of_seasons}
+												</p>
+											</div>
+										)}
+										<div className="main-block__run-time">
+											<p className="main-block__run-time-title infoTitle">
+												{type === 'Movie'
+													? 'Run time'
+													: 'Episode run time'}
+											</p>
+											<p className="main-block__run-time-value infoValue">
+												{type === 'Movie'
+													? data.runtime
+													: data.episode_run_time}{' '}
+												min
+											</p>
+										</div>
+									</div>
+
+									{type === 'tv' && (
+										<div className="main-block__right">
+											<div className="main-block__status">
+												<p className="main-block__status-title infoTitle">
+													Status
+												</p>
+												<p className="main-block__status-value infoValue">
+													{data.status}
+												</p>
+											</div>
+											<div className="main-block__last_air">
+												<p className="main-block__last_air-title infoTitle">
+													Last air date
+												</p>
+												<p className="main-block__last_air-value infoValue">
+													{data.last_air_date}
+												</p>
+											</div>
+											<div className="main-block__episodes_num">
+												<p className="main-block__episodes-title infoTitle">
+													No. of episodes
+												</p>
+												<p className="main-block__episodes-value infoValue">
+													{data.number_of_episodes}
+												</p>
+											</div>
+										</div>
+									)}
 								</div>
 								<div className="main-block__genres">
 									<p className="main-block__genres-title infoTitle">
--- src/services/TheMovieDBService.ts
+++ src/services/TheMovieDBService.ts
@@ -1,28 +1,52 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
-import { ICard } from '../models/ICard';
+import { SearchType } from '../components/UI/SearchInput/SearchInput';
 
-interface Genres {
+interface IGenres {
 	id: number;
 	name: string;
 }
-interface IMovies {
+export interface ISearchResults {
+	poster_path: string;
+	backdrop_path: string;
+	adult: boolean;
+	overview: string;
+	id: number;
+	media_type: 'movie' | 'tv' | 'person';
+	title: string;
+	name: string;
+}
+interface ISearchResponse {
 	page: number;
-	results: ICard[];
+	results: ISearchResults[];
 	total_pages: number;
 	total_results: number;
 }
-interface IMovie {
+interface IDetails {
 	id: number;
 	poster_path: string;
 	backdrop_path: string;
-	genres: Genres[];
+	genres: IGenres[];
 	title: string;
+	name: string;
 	tagline: string;
 	overview: string;
 	vote_average: number;
 	release_date: string;
 	runtime: number;
+	status: string;
+	first_air_date: string;
+	last_air_date: string;
+	number_of_seasons: number;
+	number_of_episodes: number;
+	episode_run_time: number[];
 }
+interface ISearchArgs {
+	searchType: SearchType;
+	searchTerm: string;
+	page: number;
+	include_adult: boolean;
+}
+const API_KEY: string = '918026d985ab80afa1ae6f0b53c6aa70';
 
 export const theMovieDBAPI = createApi({
 	reducerPath: 'theMovieDBAPI',
@@ -30,20 +54,39 @@ export const theMovieDBAPI = createApi({
 		baseUrl: 'https://api.themoviedb.org/3',
 	}),
 	endpoints: (build) => ({
-		popularMovies: build.query<IMovies | undefined, number>({
+		popularMovies: build.query<ISearchResponse | undefined, number>({
 			query: (page: number) => ({
 				url: '/movie/popular',
 				params: {
-					api_key: '918026d985ab80afa1ae6f0b53c6aa70',
+					api_key: API_KEY,
 					page: page,
 				},
 			}),
 		}),
-		movie: build.query<IMovie | undefined, number>({
+		movie: build.query<IDetails | undefined, number>({
 			query: (id: number) => ({
 				url: `/movie/${id}`,
 				params: {
-					api_key: '918026d985ab80afa1ae6f0b53c6aa70',
+					api_key: API_KEY,
+				},
+			}),
+		}),
+		tv: build.query<IDetails | undefined, number>({
+			query: (id: number) => ({
+				url: `/tv/${id}`,
+				params: {
+					api_key: API_KEY,
+				},
+			}),
+		}),
+		search: build.query<ISearchResponse | undefined, ISearchArgs>({
+			query: ({ searchType, searchTerm, page, include_adult }) => ({
+				url: `/search/${searchType}`,
+				params: {
+					api_key: API_KEY,
+					query: searchTerm,
+					page: page,
+					include_adult: include_adult,
 				},
 			}),
 		}),
