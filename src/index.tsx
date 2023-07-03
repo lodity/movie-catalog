58db4a0e624a9a53fda99b15d27997ab84310f0f
lodity <lodity9@gmail.com>
2023-07-03 11:50:29 +0300

    add: App wrapped in React.StrictMode

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,9 @@ const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
-	<Provider store={store}>
-		<App />
-	</Provider>
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>
 );
