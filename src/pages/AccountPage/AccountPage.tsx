1fea212e0aabb5645012c5bc87ee5e98300ee466
lodity <lodity9@gmail.com>
2023-07-11 07:54:31 +0300

    Refactoring: main page content moved to account page

--- src/pages/AccountPage/AccountPage.tsx
+++ src/pages/AccountPage/AccountPage.tsx
@@ -1,16 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
 import { IFavoriteItem } from '../../models/IFavorites';
 import { useActions } from '../../hooks/useActions';
 import Card from '../../components/UI/Card/Card';
+import BaseButton from '../../components/UI/BaseButton/BaseButton';
+import UserService from '../../services/UserService';
 
 const AccountPage = () => {
 	const { getFavorites } = useActions();
-	const { user } = useTypedSelector((state) => state.auth);
+	const { clearFavorites } = useActions();
+	const { user, isLoading } = useTypedSelector((state) => state.auth);
 	const { favorites: favoritesState } = useTypedSelector(
 		(state) => state.favorite
 	);
 	const [favorites, setFavorites] = useState<IFavoriteItem[]>([]);
+
+	async function getUsers() {
+		console.log(await UserService.fetchUsers());
+	}
 
 	useEffect(() => {
 		getFavorites(user.id);
@@ -19,20 +26,49 @@ const AccountPage = () => {
 		setFavorites(favoritesState);
 	}, [favoritesState]);
 	return (
-		<ul
-			style={{
-				margin: '0 auto',
-				width: '85%',
-				display: 'flex',
-				flexWrap: 'wrap',
-				gap: '15px',
-				justifyContent: 'center',
-			}}
-		>
-			{favorites.map((item) => (
-				<Card type={item.media_type} movie={item} key={item.id} />
-			))}
-		</ul>
+		<>
+			{isLoading ? (
+				<p>Loading...</p>
+			) : (
+				<div
+					style={{
+						margin: '0 auto',
+						display: 'flex',
+						flexDirection: 'column',
+						flexWrap: 'wrap',
+						gap: '15px',
+						alignItems: 'center',
+					}}
+				>
+					<p>Welcome back {user.username}!</p>
+					<p>
+						{user.isActivated
+							? 'Account is activated'
+							: 'Activation link has been sent to your email'}
+					</p>
+
+					<BaseButton onClick={getUsers}>getUsers</BaseButton>
+					<BaseButton onClick={() => clearFavorites(user.id, false)}>
+						clearFavorites
+					</BaseButton>
+				</div>
+			)}
+
+			<ul
+				style={{
+					margin: '0 auto',
+					width: '85%',
+					display: 'flex',
+					flexWrap: 'wrap',
+					gap: '15px',
+					justifyContent: 'center',
+				}}
+			>
+				{favorites.map((item) => (
+					<Card type={item.media_type} movie={item} key={item.id} />
+				))}
+			</ul>
+		</>
 	);
 };
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,9 @@
 import { useTypedSelector } from '../hooks/useTypedSelector';
-import BaseButton from '../components/UI/BaseButton/BaseButton';
-import UserService from '../services/UserService';
-import { useActions } from '../hooks/useActions';
 
 const HomePage = () => {
-	const { clearFavorites } = useActions();
-	const { isAuthenticated, isLoading, user } = useTypedSelector(
+	const { isAuthenticated, isLoading } = useTypedSelector(
 		(state) => state.auth
 	);
-
-	async function getUsers() {
-		console.log(await UserService.fetchUsers());
-	}
 
 	let authInformation;
 	if (isLoading) {
@@ -24,16 +16,6 @@ const HomePage = () => {
 						? 'You are authenticated'
 						: 'You are not authenticated'}
 				</p>
-				{isAuthenticated && (
-					<>
-						<p>Welcome back {user.username}!</p>
-						<p>
-							{user.isActivated
-								? 'Account is activated'
-								: 'Activation link has been sent to your email'}
-						</p>
-					</>
-				)}
 			</>
 		);
 	}
@@ -41,10 +23,6 @@ const HomePage = () => {
 	return (
 		<>
 			<h1>Hello</h1>
-			<BaseButton onClick={getUsers}>getUsers</BaseButton>
-			<BaseButton onClick={() => clearFavorites(user.id, false)}>
-				clearFavorites
-			</BaseButton>
 			{authInformation}
 		</>
 	);
